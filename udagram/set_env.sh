be9d91506ac4957617a6aac945146f1902ad307f
Mohamedaboda-bit <mohamedabod477@gmail.com>
2022-09-23 22:59:36 +0200

    3th commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -6,6 +6,6 @@ export POSTGRES_HOST=database-1.cfydayexgvrl.us-east-1.rds.amazonaws.com:5432
 export POSTGRES_DB=postgres
 export AWS_BUCKET=http://momosx.s3-website-us-east-1.amazonaws.com/
 export AWS_REGION=us-east-1
-export AWS_PROFILE=eb-cli
+export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
 export URL=http://localhost:8100
