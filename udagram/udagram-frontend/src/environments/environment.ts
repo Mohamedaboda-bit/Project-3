f6c8b4aa6cf2ebb61b15e667586f5162922d24d3
Mohamedaboda-bit <mohamedabod477@gmail.com>
2022-09-25 13:33:43 +0200

    8th commit

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://project3env-env.eba-xem6wqu2.us-east-1.elasticbeanstalk.com'
+  apiHost: 'http://project3env-env.eba-xem6wqu2.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
